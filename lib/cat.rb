a2d4aa6b6b0c716c7dd6559f9fd925d79b3cd46b
Travis Gandy <travis_gandy@yahoo.com>
2020-05-12 21:09:58 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
   # code goes here
+  attr_accessor :mood, :cat  
+  attr_reader :name, 
+  def initialize(name, owner)
+      @name = name 
+      @owner = owner
+  end 
+  def 
+  end 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
   # code goes here
+  attr_accessor :pets, :dog, :cat 
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+      @name = name  
+      @species = "human"
+      @@all << self 
+  end 
+  def say_species
+      "I am a #{@species}."
+  end 
+  def self.all 
+      @@all 
+  end 
+  def self.count
+      @@all.length 
+  end 
+  def self.reset_all
+      self.all.clear
+  end 
+  
 end
