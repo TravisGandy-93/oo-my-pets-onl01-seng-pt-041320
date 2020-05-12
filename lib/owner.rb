acd03e17317c8246285ae621d6cb33e000e099e4
Travis Gandy <travis_gandy@yahoo.com>
2020-05-12 21:46:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,7 +6,7 @@ class Owner
   def initialize(name)
       @name = name  
       @species = "human"
-      @pets = {:dogs[],:cats[]}
+      @pets = {dogs:[],cats:[]}
       @@all << self 
   end 
   def say_species
