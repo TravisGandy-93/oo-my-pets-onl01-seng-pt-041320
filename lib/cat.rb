dd8f82561dd49d492db1aa9e821b71836159a5d8
Travis Gandy <travis_gandy@yahoo.com>
2020-05-12 21:12:17 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   # code goes here
   attr_accessor :owner, :cat  
   attr_reader :name, 
-  def initialize(name, owner = Owner.new(owner))
+  def initialize(name, owner
       @name = name 
       @owner = owner
   end 
